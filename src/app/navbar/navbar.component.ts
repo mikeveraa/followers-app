e1d1587f53a14f00765739193e0e8a1b425660e6
unknown <mikeverasaragi@gmail.com>
2019-05-08 20:57:22 +0700

    percobaan

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'navbar',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  backgroundColor = environment.navBarBackgroundColor;
   constructor() { }
 
   ngOnInit() {
   }
-
 }
