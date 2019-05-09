0bab98f83c5a02469b5a97cda1ead68acacb088b
unknown <mikeverasaragi@gmail.com>
2019-05-09 22:56:45 +0700

    cobacoba

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 
 const express = require('express');
 const app = express();
+const app = express();
+app.use(express.static(__dirname + '/cobahosting'));
 
-app.use(express.static(__dirname + '/dist'));
-
-app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html');
+// app.all('*', (req, res) => {
+//   res.status(200).sendFile(__dirname + '/dist/index.html');
+// });
+app.use(express.static(__dirname + '/cobahosting'));
+app.get('/*', function(req,res){
+  res.sendFile(path.join(__dirname + 'cobahosting/index.hrml'));
 });
-
 app.listen(process.env.PORT || 8080);
